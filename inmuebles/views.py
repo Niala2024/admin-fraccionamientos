310485a4756529874620a2d19c5cbe15dbcc50e1
Alain <alaincip@gmail.com>
2026-01-28 17:58:35 -0600

    Restaurar serializer de casas con logica de propietarios

--- inmuebles/views.py
+++ inmuebles/views.py
@@ -1,22 +1,34 @@
-from rest_framework import viewsets, permissions
-from .models import Casa, Reservacion, Amenidad
-from .serializers import CasaSerializer, ReservacionSerializer, AmenidadSerializer
+from rest_framework import viewsets, exceptions
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+from .models import Casa, Fraccionamiento, Calle
+from .serializers import CasaSerializer, FraccionamientoSerializer, CalleSerializer
 
-# ✅ ESTA ES LA CLASE QUE BUSCABA EL SERVIDOR
-class InmuebleViewSet(viewsets.ModelViewSet):
-    queryset = Casa.objects.all()
+class FraccionamientoViewSet(viewsets.ModelViewSet):
+    serializer_class = FraccionamientoSerializer
+    parser_classes = (MultiPartParser, FormParser, JSONParser)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Fraccionamiento.objects.all().order_by('nombre')
+        
+        if hasattr(user, 'fraccionamiento_administrado') and user.fraccionamiento_administrado:
+            return Fraccionamiento.objects.filter(id=user.fraccionamiento_administrado.id).order_by('nombre')
+            
+        if hasattr(user, 'casa') and user.casa:
+            return Fraccionamiento.objects.filter(id=user.casa.fraccionamiento.id).order_by('nombre')
+            
+        return Fraccionamiento.objects.none()
+
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            raise exceptions.PermissionDenied("Solo el Super Administrador puede registrar nuevos fraccionamientos.")
+        return super().create(request, *args, **kwargs)
+
+class CalleViewSet(viewsets.ModelViewSet):
+    queryset = Calle.objects.all().order_by('nombre')
+    serializer_class = CalleSerializer
+
+class CasaViewSet(viewsets.ModelViewSet):
+    queryset = Casa.objects.all().order_by('calle__nombre', 'numero_exterior')
     serializer_class = CasaSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class ReservacionAmenidadViewSet(viewsets.ModelViewSet):
-    queryset = Reservacion.objects.all()
-    serializer_class = ReservacionSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(usuario=self.request.user)
-
-class AmenidadViewSet(viewsets.ModelViewSet):
-    queryset = Amenidad.objects.all()
-    serializer_class = AmenidadSerializer
-    permission_classes = [permissions.IsAuthenticated]
